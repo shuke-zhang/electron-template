32146064a3a9128d41e48dc4549368c61a5bb8c5
shuke <zhangenjian207@gmail.com>
2025-06-16 15:06:59 +0800

    解决控制台报错

--- src/renderer/src/router/setupRouterGuard/createPermissionGuard.ts
+++ src/renderer/src/router/setupRouterGuard/createPermissionGuard.ts
@@ -1,39 +1,35 @@
 import type { Router } from 'vue-router';
-
-import { getCacheToken } from '@/utils/cache';
+import { getCacheToken, removeCacheToken } from '@/utils/cache';
 
 export function createPermissionGuard(router: Router) {
   router.beforeEach(async (_to, _from, next) => {
     const userStore = useUserStore();
-    if (getCacheToken()) {
+    const token = getCacheToken();
+
+    if (token) {
       if (!userStore.userName) {
         try {
           await userStore.getInfo();
-          next({
-            path: _to.path,
-            replace: true,
-          });
+          return next({ path: _to.path, replace: true });
         }
-        catch (_error) {
+        catch {
           removeCacheToken();
-          next('/login');
+          return next('/login');
         }
       }
-      if (_to.path === '/login') {
-        next('/');
-      }
-      else {
-        console.log('有token，去的是其他页面放行');
 
-        next();
+      if (_to.path === '/login') {
+        return next('/');
       }
+
+      return next();
     }
     else {
       if (_to.path === '/login') {
-        next();
+        return next();
       }
       else {
-        next('/login');
+        return next('/login');
       }
     }
   });
